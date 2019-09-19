8ba776b191bfff419c05c5121b5927adc99e7618
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-18 21:15:05 -0400

    Added initial mobile navigation styling and js for accordion menu

---
+++ index.js
@@ -0,0 +1,7 @@
+const mobileAccordion = document.querySelector('.mobile-nav');
+
+mobileAccordion.addEventListener('click', () => {
+    const menu = document.querySelector('#top-nav');
+    const menuState = menu.style.visibility;
+    return !menuState || menuState === "hidden" ? menu.style.visibility = "visible" : menu.style.visibility = "hidden";
+})
