b9f3c8e7a8247929b23b4271fbf0268755d5218a
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-19 22:02:28 -0400

    Added previewed logo and changed text format of mobile hero section

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const mobileAccordion = document.querySelector('.mobile-nav');
+const menu = document.querySelector('#top-nav');
+const menuState = () => menu.style.visibility;
 
 mobileAccordion.addEventListener('click', () => {
-    const menu = document.querySelector('#top-nav');
-    const menuState = menu.style.visibility;
-    return !menuState || menuState === "hidden" ? menu.style.visibility = "visible" : menu.style.visibility = "hidden";
+
+    return !menuState() || menuState() === "hidden" ? menu.style.visibility = "visible" : menu.style.visibility = "hidden";
 });
+
+
+[document.querySelector('main'), document.querySelector('footer')].forEach(node => {
+    node.addEventListener('click', () => {
+        if(!menuState() || menuState() === 'visible') menu.style.visibility = "hidden";
+        return;
+    });
+})
